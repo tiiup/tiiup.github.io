a8e1a06770f80367f097c74e01cde852ac6c0ec5
Tiia Puumalainen <tiia.puumalainen@edu.karelia.fi>
2023-03-19 22:19:37 +0200

    Korjailua

--- JavaScript/navscript.js
+++ JavaScript/navscript.js
@@ -2,24 +2,22 @@ function toggleMenu() {
   const menu = document.getElementById("menu");
   menu.classList.toggle("show");
 }
-
+/*Pitäisi vaihtaa buttonin väriä kun se on valittu, mutta tällä hetkellä ei toimi*/
 document.addEventListener("DOMContentLoaded", () => {
-  const ham = document.querySelector(".ham");
-  ham.addEventListener("click", toggleMenu);
-});
+  // Get all the buttons
+  const buttons = document.querySelectorAll('.button-9');
 
-// Get all the buttons
-const buttons = document.querySelectorAll('.button-9');
+  // Loop through the buttons and add an event listener to each one
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      // Remove the "selected" class from all buttons
+      buttons.forEach(button => {
+        button.classList.remove('selected');
+      });
 
-// Loop through the buttons and add an event listener to each one
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    // Remove the "selected" class from all buttons
-    buttons.forEach(button => {
-      button.classList.remove('selected');
+      // Add the "selected" class to the clicked button
+      button.classList.add('selected');
+      console.log(buttons);
     });
-
-    // Add the "selected" class to the clicked button
-    button.classList.add('selected');
   });
 });
